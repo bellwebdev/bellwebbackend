32f43f78b15c90477144af4a2a06e1af15078e47
BellWebDev <bellwebdev@Franciss-MacBook-Pro.local>
2025-09-26 12:35:07 -0400

    debug log for render

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 database = databases.Database(DATABASE_URL)
 app = FastAPI()
 
+print("DATABASE_URL:", DATABASE_URL)
 
 @app.get("/health")
 async def health():
